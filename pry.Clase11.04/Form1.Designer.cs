19baaa02953a54bf3dfd1eaced15c065c5f3c6f8
Valentin Lopez <112416951+Valenlopez14@users.noreply.github.com>
2023-04-11 11:54:53 -0300

    Add files via upload
    Treeview e ImageList

---
+++ pry.Clase11.04/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace pry.Clase11._04
+{
+    partial class frmTreeView
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTreeView));
+            this.tv = new System.Windows.Forms.TreeView();
+            this.il = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // tv
+            // 
+            this.tv.ImageIndex = 0;
+            this.tv.ImageList = this.il;
+            this.tv.Location = new System.Drawing.Point(12, 12);
+            this.tv.Name = "tv";
+            this.tv.SelectedImageIndex = 0;
+            this.tv.Size = new System.Drawing.Size(774, 438);
+            this.tv.TabIndex = 0;
+            this.tv.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tv_NodeMouseClick);
+            // 
+            // il
+            // 
+            this.il.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("il.ImageStream")));
+            this.il.TransparentColor = System.Drawing.Color.Transparent;
+            this.il.Images.SetKeyName(0, "Abraham.png");
+            this.il.Images.SetKeyName(1, "Homero.jpg");
+            this.il.Images.SetKeyName(2, "marge.png");
+            this.il.Images.SetKeyName(3, "Barto.png");
+            // 
+            // frmTreeView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 450);
+            this.Controls.Add(this.tv);
+            this.Name = "frmTreeView";
+            this.Text = "Uso de TreeView";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tv;
+        private System.Windows.Forms.ImageList il;
+    }
+}
+
---
+++ pry.Clase11.04/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pry.Clase11._04
+{
+    public partial class frmTreeView : Form
+    {
+        public frmTreeView()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Declarar nodos en el load
+            TreeNode abuelo;
+            TreeNode padre;
+            
+            TreeNode hijo;
+
+            //Declarar nodo raiz en el TreeView y adjuntarle el icono correspondiente
+            abuelo = tv.Nodes.Add("ALIMENTOS","ALIMENTOS", 0);
+
+            //Declarar los otros dos nodos en el TreeView
+            padre = abuelo.Nodes.Add("FRUTAS", "FRUTAS", 1,2);
+            //Agrego los nodos dependientes de Frutas
+            hijo = padre.Nodes.Add("Uva","Uva", 3,2);
+            //Darle informacion a la uva con el Tag
+            hijo.Tag = "380";
+            hijo = padre.Nodes.Add("Pera", "Uva", 3,2);
+            hijo.Tag = "400";
+            hijo = padre.Nodes.Add("Naranja", "Naranja", 3,2);
+            hijo.Tag = "500";
+
+
+
+            padre = abuelo.Nodes.Add("VERDURAS", "VERDURAS", 1);
+
+            hijo = padre.Nodes.Add("Acelga","Acelga",3,2);
+            hijo.Tag = "250";
+            hijo = padre.Nodes.Add("Lechuga","Lechuga", 3,2);
+            hijo.Tag = "175";
+            hijo = padre.Nodes.Add("Repollo", "Repollo", 3,2);
+            hijo.Tag = "200";
+
+
+
+
+
+        }
+
+        private void tv_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            //Mostrar nodo elegido
+            if (e.Node.Level==2)
+            {
+                MessageBox.Show("EL KILO VALE $"+ ""+ e.Node.Tag.ToString());
+            }
+
+            
+        }
+    }
+}
